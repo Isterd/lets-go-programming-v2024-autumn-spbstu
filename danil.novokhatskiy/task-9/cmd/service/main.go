cdf4228ea7439451dd71c2672c445d62a1874736
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-11-21 16:57:57 +0300

    [TASK-9] Add config for my program

---
+++ danil.novokhatskiy/task-9/cmd/service/main.go
@@ -0,0 +1,21 @@
+package main
+
+type Contact struct {
+	name  string
+	phone string
+}
+
+var testTable []Contact = []Contact{
+	{
+		name:  "Nikita",
+		phone: "89289019785",
+	},
+	{
+		name:  "",
+		phone: "",
+	},
+}
+
+func main() {
+
+}
